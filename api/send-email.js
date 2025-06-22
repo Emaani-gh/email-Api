3424f36cf957ed0fd6c99cc3f03c8ce9ba404db0
Seidu Abdul Salam <90971780+Emaani-gh@users.noreply.github.com>
2025-06-22 15:45:11 +0300

    Api (#2)
    * email api
    
    * update api

---
+++ api/send-email.js
@@ -0,0 +1,41 @@
+import express from "express";
+import { Resend } from "resend";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+  })
+);
+app.use(express.json());
+
+app.post("/send-email", async (req, res) => {
+  const { name, email, message } = req.body;
+
+  try {
+    const data = await resend.emails.send({
+      from: process.env.FROM_EMAIL,
+      to: process.env.TO_EMAIL,
+      subject: "message from portfolio site",
+      html: `
+        <p><strong>Name:</strong> ${name}</p>
+        <p><strong>Email:</strong> ${email}</p>
+        <p><strong>Message:<br/>${message}</p>
+      `,
+    });
+
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: "Email failed", details: error.message });
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
